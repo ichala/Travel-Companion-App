fd3d3042ad07a5ef2999bd8a1285141e470adc33
ichala <alijandhd@gmail.com>
2022-02-09 16:48:23 +0100

    added a demo places to test mapping

--- src/Components/List/List.jsx
+++ src/Components/List/List.jsx
@@ -1,32 +1,58 @@
-import { FormControl, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
-import React, { useState } from 'react'
+import {
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography,
+} from "@material-ui/core";
+import React, { useState } from "react";
+import Places from "../Places/Places";
 import useStyles from "./styles";
 function List() {
-  const classes =useStyles();
-  const [type,setType]=useState('restaurants')
-  const [rating,setRating]=useState('')
+  const classes = useStyles();
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
+  const places = [
+    { name: "Cool Place" },
+    { name: "Cool Place" },
+    { name: "Cool Place" },
+    { name: "Cool Place" },
+    { name: "Cool Place" },
+    { name: "Cool Place" },
+  ];
   return (
     <div className={classes.container}>
-      <Typography variant='h4' > Nearby Hotels , Restaurants & Leisures .. </Typography>
+      <Typography variant="h4">
+        {" "}
+        Nearby Hotels , Restaurants & Leisures ..{" "}
+      </Typography>
       <FormControl className={classes.formControl}>
-<InputLabel>Type</InputLabel>
-<Select value={type} onChange={e=>setType(e.target.value)}>
-<MenuItem value="restaurants">Restaurants</MenuItem>
-<MenuItem value="hotels">Hotels</MenuItem>
-<MenuItem value="leisures">Leisures</MenuItem>
-</Select>
+        <InputLabel>Type</InputLabel>
+        <Select value={type} onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="leisures">Leisures</MenuItem>
+        </Select>
       </FormControl>
       <FormControl className={classes.formControl}>
-<InputLabel>Rating</InputLabel>
-<Select value={rating} onChange={e=>setRating(e.target.value)}>
-<MenuItem value={0}>All</MenuItem>
-<MenuItem value={3}>Above 3.0</MenuItem>
-<MenuItem value={4}>Above 4.0</MenuItem>
-<MenuItem value={4.5}>Above 4.5</MenuItem>
-</Select>
+        <InputLabel>Rating</InputLabel>
+        <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
       </FormControl>
+      <Grid container spacing={3} className={classes.list}>
+        {places?.map((place, i) => (
+          <Grid item key={i} xs={12}>
+          <Places place={place} />
+          </Grid>
+        ))}
+      </Grid>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
--- src/Components/Places/Places.jsx
+++ src/Components/Places/Places.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Places() {
+function Places({place}) {
   return (
-    <div>Places</div>
+    <div>{place.name}</div>
   )
 }
 
