4fc6286783cde28a4352e05bca45dc923a59c23f
ichala <alijandhd@gmail.com>
2022-02-09 15:14:36 +0100

    init Materials Grid Sysytem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import React from "react";
 
-
+import Header from "./Components/Header/Header";
+import List from "./Components/List/List";
+import Map from "./Components/Map/Map";
+import Places from "./Components/Places/Places";
+import {CssBaseline ,Grid} from '@material-ui/core'
 function App() {
   return (
     <div className="App">
-   
+      
+      <CssBaseline />
+      <Header />
+        <Grid container spacing={3} style={{width :'100%'}}>
+          <Grid item xs={12} md={4}>
+            <List />
+          </Grid>
+          <Grid item xs={12} md={8}>
+            <Map />
+          </Grid>
+        </Grid>
+      
+      {/* 
+      <Map />
+      <Places /> */}
     </div>
   );
 }
